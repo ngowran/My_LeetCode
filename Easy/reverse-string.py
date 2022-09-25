faea0ed76046b59e1a760c05c47f1c50e9eb91b3
Niamh Gowran <91330763+ngowran@users.noreply.github.com>
2022-09-25 22:13:55 +0100

    Create reverse-string.py

---
+++ Easy/reverse-string.py
@@ -0,0 +1,16 @@
+#Link: https://leetcode.com/problems/reverse-string/
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) -1
+        
+        while left < right:
+            tmp = s[left]
+            s[left] = s[right]
+            s[right] = tmp
+            left += 1
+            right -= 1
+        return s
